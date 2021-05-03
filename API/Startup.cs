5a4070cf4e09b03e73df54c5ba1aa4edab081b59
Irene Lambrou <ilambrou@i-spiral.com>
2021-05-03 22:59:29 +0300

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -38,6 +38,7 @@ namespace API
 
             });
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -54,9 +55,13 @@ namespace API
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
             }
 
+           
             app.UseHttpsRedirection();
-
+            
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200") );
+            
 
             app.UseAuthorization();
 
